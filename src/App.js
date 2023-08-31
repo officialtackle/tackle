a876edf7a2153677526c1331a0a0c0a8e365c071
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2023-08-31 16:35:44 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './components/Header';
+import "./App.css"
+import Main from './components/Main';
+import Roadmap from './components/Roadmap';
+import Token from './components/Token';
+import FaQ from './components/FaQ';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Roadmap />
+      <Token />
+      <FaQ />
+      
+    </div>
+  )
+}
+
+export default App
